6f564fd73b64b6ce006e848930609c6cb4e87b95
Noor Haj Dawood <noorhajdawood@gmail.com>
2023-03-01 17:19:53 +0200

    cors

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const {authRouter} = require("./routers/authRouter");
-const {projectRouter} = require("./routers/projectRouter");
-const {userRouter} = require("./routers/userRouter");
-const winston = require('winston');
-const moment=require('moment');
+const { authRouter } = require("./routers/authRouter");
+const { projectRouter } = require("./routers/projectRouter");
+const { userRouter } = require("./routers/userRouter");
+const winston = require("winston");
+const moment = require("moment");
 const path = require("path");
 const { networkRouter } = require("./routers/networkRouter");
 const { edgeRouter } = require("./routers/edgeRouter");
@@ -16,29 +16,42 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 const logger = winston.createLogger({
-    level: 'debug',
-    format: winston.format.simple(),
-    transports: [
-        new winston.transports.File({ filename: 'logs/dev.log' }),
-    ],
+  level: "debug",
+  format: winston.format.simple(),
+  transports: [new winston.transports.File({ filename: "logs/dev.log" })],
 });
 
-app.use('/', (req, res,next) => {
-    logger.info(` ${req.method} |  ${req.url}  | ${ moment()}  `);
-    next();
+app.use("/", (req, res, next) => {
+  logger.info(` ${req.method} |  ${req.url}  | ${moment()}  `);
+  next();
 });
 
-app.use('/api/auth', authRouter);
-app.use('/api/projects', projectRouter);
-app.use('/api/users', userRouter);
-app.use('/api/networks', networkRouter);
-app.use('/api/edges', edgeRouter);
-app.use('/api/nodes', nodeRouter);
-app.use('/api/timeRanges', timeRangeRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/projects", projectRouter);
+app.use("/api/users", userRouter);
+app.use("/api/networks", networkRouter);
+app.use("/api/edges", edgeRouter);
+app.use("/api/nodes", nodeRouter);
+app.use("/api/timeRanges", timeRangeRouter);
 
 app.use((req, res) => {
-    res.status(404).send(`Page not found`);
+  res.status(404).send(`Page not found`);
 });
 
-app.listen(port, () => console.log(`Express server is running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Express server is running on port ${port}`)
+);
