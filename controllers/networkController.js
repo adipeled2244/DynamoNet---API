e10f61f44f32eb3868f13cc45f6b7e896b93f2bd
adi peled <adipeled224@gmail.com>
2023-03-07 12:03:03 +0200

    conflicts

--- controllers/networkController.js
+++ controllers/networkController.js
@@ -85,7 +85,6 @@ exports.networkController = {
     const networkParams = req.body;
     let updateResult;
 
-<<<<<<< HEAD
         try {
             updateResult = await networkService.updateNetwork(networkIdParam,networkParams);
             if (updateResult.matchedCount == 1) {
@@ -112,53 +111,14 @@ exports.networkController = {
         }
     },
     
-=======
-    try {
-      updateResult = await networkService.updateNetwork(
-        networkIdParam,
-        networkParams
-      );
-      if (updateResult.matchedCount == 1) {
-        return res.status(200).json({ message: "Network updated" });
-      } else {
-        return res.status(404).json({ error: "Network id not found" });
-      }
-    } catch (err) {
-      res
-        .status(500)
-        .json({ error: `Error update network ${networkIdParam} : ${err}` });
-      return;
-    }
-  },
-  async deleteNetwork(req, res) {
-    logger.info(`[deleteNetwork] - ${path.basename(__filename)}`);
-    const networkIdParam = req.params.networkId;
-    let deleteResult;
-    try {
-      deleteResult = await networkService.deleteNetwork(networkIdParam);
-      return res.status(200).json({ message: `Network deleted` });
-    } catch (err) {
-      res
-        .status(500)
-        .json({ error: `Error deleting Network ${networkIdParam} : ${err}` });
-      return;
-    }
-  },
-
->>>>>>> 44dc92513dc9a0458c46cc3aa05d988637f8a1c7
   async getNetworkEdgesByType(req, res) {
     logger.info(`[getNetworkEdgesByType] - ${path.basename(__filename)}`);
     const networkIdParam = req.params.networkId;
     const edgeType = req.query.type;
     let network;
     network = await networkService.getNetwork(networkIdParam);
-<<<<<<< HEAD
     if(!network){
         return res.status(404).json({ error: `Cannot get network's edges` });
-=======
-    if (!network) {
-      return res.status(404).json({ error: "Network id not found" });
->>>>>>> 44dc92513dc9a0458c46cc3aa05d988637f8a1c7
     }
     networkEdges = network.edges;
     try {
@@ -167,15 +127,8 @@ exports.networkController = {
       });
       res.status(200).json({ filterEdges });
     } catch (err) {
-<<<<<<< HEAD
         res.status(500).json({ error: `Cannot get network's edges, please try again later` });
         return;
-=======
-      res.status(500).json({
-        error: `Error get edges from type ${type} for projectId ${projectIdParam} : ${err}`,
-      });
-      return;
->>>>>>> 44dc92513dc9a0458c46cc3aa05d988637f8a1c7
     }
   },
 };
